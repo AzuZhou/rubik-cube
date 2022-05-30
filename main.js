9bc959cf3fcb5b2f878ded5df448d0e30c192110
Azucena Zhou <abbey-1825@hotmail.com>
2022-05-30 16:14:45 +0200

    add eslint and create initial cube

--- main.js
+++ main.js
@@ -1,22 +1,71 @@
-import './style.css'
+import './style.css';
 
-import * as THREE from "three"
+import * as THREE from 'three';
 
-const scene = new THREE.Scene()
+const yellow  = new THREE.Color(0xe2f705);
+const green  = new THREE.Color(	0xa3f307);
+const blue  = new THREE.Color(0x05f9e2);
+const pink  = new THREE.Color(0xf50b86);
+const orange  = new THREE.Color(0xff6f00);
+const black = new THREE.Color(0x1D1F33);
+const white = new THREE.Color(0xffffff);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-camera.position.setZ(2)
+const scene = new THREE.Scene();
+scene.background = white;
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.setZ(7);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.getElementById('webgl'),
-})
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
+// TODO: create cubes using BufferGeometry
+const createLane = () => {
+  const group = new THREE.Group();
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: black, wireframe: true});
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+  for (let i = 0; i < 3; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = i;
+    group.add(mesh);
+  }
 
-renderer.render(scene, camera)
+  return group;
+};
+
+const createSide = () => {
+  const group = new THREE.Group();
+  
+  for (let i = 0; i < 3; i++) {
+    const lane = createLane()
+    lane.position.y = i;
+    group.add(lane);
+  }
+
+  return group
+}
+
+const createCube = () => {
+  const group = new THREE.Group();
+
+  for (let i = 0; i < 3; i++) {
+    const side = createSide()
+    side.position.z = i;
+    group.add(side);
+  }
+
+  return group;
+};
+
+const cube = createCube();
+
+cube.rotation.x = Math.PI * 0.25;
+cube.rotation.y = Math.PI * 0.25;
+scene.add(cube);
+// use quaternions
+
+renderer.render(scene, camera);
